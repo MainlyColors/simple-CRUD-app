1c0e8dc08c46d809276c04f14a5ab1ee09cffaab
Ryan <ryanmahara@gmail.com>
2022-06-01 11:43:18 -0500

    add read operation for index.html in server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log('May Node be with you');
+const express = require('express');
+const app = express();
+
+app.listen(8000, function () {
+  console.log('listening on 8000');
+});
+
+// We normally abbreviate `request` to `req` and `response` to `res`.
+// "/" is the endpoint on the home page aka http://localhost:8000
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
